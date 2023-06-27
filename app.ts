3b02f7b83847b04b360a7f6b3cdc6bf367ffac71
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-06-27 00:10:16 -0500

    feat: implement call dynamic routes

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import dbConnect from './config/mongoose';
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
-
+app.use('/api', require('./routes/index.r'));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
