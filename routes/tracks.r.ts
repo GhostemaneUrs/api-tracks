f5e487d3d2261850d4ac54a4d8f9fd28b6941bba
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-02 01:54:45 -0500

    feat: implement validators create tracks

--- routes/tracks.r.ts
+++ routes/tracks.r.ts
@@ -5,11 +5,12 @@ import {
   updateTrack,
   createTrack,
 } from '../controllers/tracks';
+import { createTrackValidator } from '../validators/tracks';
 
 const router = express.Router();
 router.get('/', getTracks);
 router.get('/:id', getTrack);
-router.post('/', createTrack);
+router.post('/', createTrackValidator, createTrack);
 router.put('/:id', updateTrack);
 
 module.exports = router;
