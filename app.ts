c2c894004349f5d32507cdb539b17359c7e2d9a3
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-01 23:51:40 -0500

    feat: implement read json body

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import dbConnect from './config/mongoose';
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use('/api', router);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
