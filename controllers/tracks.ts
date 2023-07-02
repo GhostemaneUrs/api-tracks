0c07d4aa244cc1dc5573389a361e731a98fd333d
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-01 23:51:28 -0500

    feat: implement create tracks and get tracks

--- controllers/tracks.ts
+++ controllers/tracks.ts
@@ -6,12 +6,18 @@ export const getTrack = async (req: Request, res: Response): Promise<void> => {
   res.send({ data });
 };
 
-export const getTracks = (req: Request, res: Response): void => {
-  res.send({ message: 'Hello from tracks' });
+export const getTracks = async (req: Request, res: Response): Promise<void> => {
+  const data = await tracks.find({});
+  res.send({ data });
 };
 
-export const createTrack = (req: Request, res: Response): void => {
-  res.send({ message: 'Hello from tracks' });
+export const createTrack = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { body } = req;
+  const data = await tracks.create(body);
+  res.send({ data });
 };
 
 export const updateTrack = (req: Request, res: Response): void => {
--- routes/tracks.r.ts
+++ routes/tracks.r.ts
@@ -1,8 +1,15 @@
 import express from 'express';
-import { getTrack, getTracks } from '../controllers/tracks';
+import {
+  getTrack,
+  getTracks,
+  updateTrack,
+  createTrack,
+} from '../controllers/tracks';
 
 const router = express.Router();
 router.get('/', getTracks);
 router.get('/:id', getTrack);
+router.post('/', createTrack);
+router.put('/:id', updateTrack);
 
 module.exports = router;
