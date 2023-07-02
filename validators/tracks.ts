ab02d8c907fd93b89e776f4cdf24fbf51efe4ffe
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-02 01:52:58 -0500

    feat: create validators tracks with express validator

---
+++ validators/tracks.ts
@@ -0,0 +1,19 @@
+import { check } from 'express-validator';
+import { validateResult } from '../utils/files';
+import { NextFunction, Response, Request } from 'express';
+
+export const createTrackValidator = [
+  check('name').exists().notEmpty(),
+  check('album').exists().notEmpty(),
+  check('cover').exists().notEmpty(),
+  check('artist').exists().notEmpty(),
+  check('duration').exists().notEmpty(),
+  check('duration.end').exists().notEmpty(),
+  check('duration.start').exists().notEmpty(),
+  check('artist.name').exists().notEmpty(),
+  check('artist.nickname').exists().notEmpty(),
+  check('artist.nationality').exists().notEmpty(),
+  check('mediaId').exists().notEmpty().isMongoId(),
+  (req: Request, res: Response, next: NextFunction) =>
+    validateResult(req, res, next),
+];
