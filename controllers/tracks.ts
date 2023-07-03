119565e2f13c9cea32a7c087b2a3e35e52c7cdf2
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-02 22:55:36 -0500

    feat: implement response utils and clean body

--- controllers/tracks.ts
+++ controllers/tracks.ts
@@ -1,23 +1,36 @@
 import tracks from '../models/tracks';
 import { Request, Response } from 'express';
+import { matchedData } from 'express-validator';
+import { responseError, responseSuccess } from '../utils/response';
 
 export const getTrack = async (req: Request, res: Response): Promise<void> => {
-  const data = await tracks.find({});
-  res.send({ data });
+  try {
+    const data = await tracks.find({});
+    res.send({ data });
+  } catch (error) {
+    // httpError(res, error?.message, 403);
+  }
 };
 
 export const getTracks = async (req: Request, res: Response): Promise<void> => {
-  const data = await tracks.find({});
-  res.send({ data });
+  try {
+    const data = await tracks.find({});
+    responseSuccess(res, 'Tracks retrieved successfully', data);
+  } catch (error) {
+    responseError(res, 'Failed to retrieve tracks', `${error}`);
+  }
 };
 
 export const createTrack = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const { body } = req;
-  const data = await tracks.create(body);
-  res.send({ data });
+  try {
+    const data = await tracks.create(matchedData(req, { locations: ['body'] }));
+    responseSuccess(res, 'Track created successfully', data, 201);
+  } catch (error) {
+    responseError(res, 'Failed to create track', `${error}`);
+  }
 };
 
 export const updateTrack = (req: Request, res: Response): void => {
