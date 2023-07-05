c65986fd3a0a977609aff7b230d3b1c9a3b39e2e
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-04 22:18:12 -0500

    feat: implement call method in the routes storage

--- routes/storages.r.ts
+++ routes/storages.r.ts
@@ -1,8 +1,19 @@
 import express from 'express';
-import { uploadFile } from '../controllers/storages';
 import { uploadMiddleware } from '../middleware/storage';
+import {
+  getFile,
+  getFiles,
+  deleteFile,
+  uploadFile,
+  deletePhysicalFile,
+} from '../controllers/storages';
+import { idStorageValidator } from '../validators/storages';
 
 const router = express.Router();
+router.get('/', getFiles);
+router.get('/:id', idStorageValidator, getFile);
+router.delete('/:id', idStorageValidator, deleteFile);
+router.delete('/:id', idStorageValidator, deletePhysicalFile);
 router.post('/', uploadMiddleware.single('file'), uploadFile);
 
 module.exports = router;
