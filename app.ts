1352fa97bf09017cc4aaaae6b605c1eca8542f0c
Andres Jaramillo <JaramilloG1999@outlook.com>
2023-07-04 23:27:35 -0500

    feat: update structure folders

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import router from './routes/index.r';
-import dbConnect from './config/mongoose';
+import router from './src/routes/index.r';
+import dbConnect from './src/config/mongoose';
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
